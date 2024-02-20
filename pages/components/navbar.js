31c8d3a336b7bfe71cf11bd0614e5dd3055e2b82
Kanna116 <muralikanna77@gmail.com>
2024-02-20 19:05:20 +0530

    basic navbar created

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import Logo from './logo'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='w-full h-[60px] p-[10px] fixed top-[10px] left-0 lg:px-[96px] md:px-[64px] px-[24px] flex items-center justify-between'>
+      <div className='h-full w-fit'>
+        <Logo />
+      </div>
+      <ul className='lg:flex md:flex items-center gap-5 bg-zinc-100 w-fit text-black px-5 py-1 rounded text-xs hidden'>
+        <li className='cursor-pointer'>Home</li>
+        <li className='cursor-pointer'>About</li>
+        <li className='cursor-pointer'>Projects</li>
+        <li className='cursor-pointer'>Contact</li>
+      </ul>
+
+
+      
+
+    </div>
   )
 }
 
--- pages/home/index.js
+++ pages/home/index.js
@@ -3,9 +3,8 @@ import Navbar from '../components/navbar'
 
 const HomePage = () => {
   return (
-    <div className='px-[24px] md:px-[64px] lg:px-[96px] bg-red-300 min-h-screen w-full'>
+    <div className='px-[24px] md:px-[64px] lg:px-[96px] min-h-screen w-full'>
         <Navbar />
-
     </div>
   )
 }
