d597f9d5f035afdb636067ed18dd0cecf8eafc1f
Kanna116 <muralikanna77@gmail.com>
2024-02-20 09:04:57 +0530

    font link moved to document.js file for ssr

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="https://api.fontshare.com/v2/css?f[]=poppins@2,900,901,701,801,201,401,301,500,400,601,100,1,300,501,101,800,700,200,600&f[]=satoshi@1,900,700,500,301,701,300,501,401,901,400,2&display=swap" rel="stylesheet"></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import HomePage from './home'
+import Head from 'next/head'
 
 const Home = () => {
   return (
     <>
-    <head>
+    <Head>
       <title>Klaxe</title>
-      <link href="https://api.fontshare.com/v2/css?f[]=poppins@2,900,901,701,801,201,401,301,500,400,601,100,1,300,501,101,800,700,200,600&f[]=satoshi@1,900,700,500,301,701,300,501,401,901,400,2&display=swap" rel="stylesheet"></link>
-    </head>
+    </Head>
+
     <HomePage />
     </>
   )
