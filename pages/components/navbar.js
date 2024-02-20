d32b7d7d114798c2a2987df3e5eadad5ac777b1c
Kanna116 <muralikanna77@gmail.com>
2024-02-20 19:31:14 +0530

    menu icon made and simple animation is given

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from './logo'
+import Image from 'next/image';
 
 function Navbar() {
+
+  const [isMenuOpen, SetIsMenuOpen] = useState(true);
   return (
     <div className='w-full h-[50px] p-[10px] fixed top-[10px] left-0 lg:px-[96px] md:px-[64px] px-[24px] flex items-center justify-between'>
       <div className='h-full w-fit'>
@@ -16,12 +19,26 @@ function Navbar() {
 
       {/* menu for mobile  */}
 
-      <div className='group w-10 aspect-square bg-white rounded-full flex items-center justify-evenly  flex-col p-[10px]'>
-        <div className='w-2/3 h-[2px] bg-black group-hover:w-full duration-200'></div>
-        <div className='w-full h-[3px] bg-black'></div>
-        <div className='w-2/3 h-[2px] bg-black group-hover:w-full duration-200'></div>
+      <div className='w-10 aspect-square bg-white rounded-full flex items-center justify-evenly gap-[1px]  flex-col p-[10px]'
+        onClick={() => SetIsMenuOpen(prevValue => !prevValue)}
+      >
+        <div className='w-5 shrink-0 h-[2px] bg-black duration-200' style={{ transform: (isMenuOpen) ? `translate(0px,6px) rotate(-45deg)` : `translate(0px,0px) rotate(0deg)` }}></div>
+        <div className='w-5 shrink-0 h-[2px] bg-black duration-300 origin-center ' style={{ transform: (isMenuOpen) ? `scaleX(0)` : `scaleX(1)` }}></div>
+        <div className='w-5 shrink-0 h-[2px] bg-black duration-200' style={{ transform: (isMenuOpen) ? `translate(0px,-6px) rotate(45deg)` : `translate(0px,0px) rotate(0deg)` }}></div>
       </div>
 
+
+      <ul className="second-menu fixed t-[50vh] bg-red-100 w-[250px] h-[250px]">
+        <div className="back">
+          <Image
+           
+          />
+        </div>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
 
 
     </div>
