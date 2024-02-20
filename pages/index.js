ad8b2201db71a6e1d986ce19112af4fc4ae60023
Kanna116 <muralikanna77@gmail.com>
2024-02-20 09:00:14 +0530

    font family added

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const Home = () => {
     <>
     <head>
       <title>Klaxe</title>
-      
+      <link href="https://api.fontshare.com/v2/css?f[]=poppins@2,900,901,701,801,201,401,301,500,400,601,100,1,300,501,101,800,700,200,600&f[]=satoshi@1,900,700,500,301,701,300,501,401,901,400,2&display=swap" rel="stylesheet"></link>
     </head>
     <HomePage />
     </>
