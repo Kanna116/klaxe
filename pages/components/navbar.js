910729f2fd6cdc3d14ae1fe24773fb051d8d3e96
Kanna116 <muralikanna77@gmail.com>
2024-02-20 19:58:15 +0530

    animation idea revoked

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -19,26 +19,13 @@ function Navbar() {
 
       {/* menu for mobile  */}
 
-      <div className='w-10 aspect-square bg-white rounded-full flex items-center justify-evenly gap-[1px]  flex-col p-[10px]'
+      <div className='w-10 aspect-square bg-white rounded-full flex items-center justify-evenly gap-[1px]  flex-col p-[10px] z-50'
         onClick={() => SetIsMenuOpen(prevValue => !prevValue)}
       >
         <div className='w-5 shrink-0 h-[2px] bg-black duration-200' style={{ transform: (isMenuOpen) ? `translate(0px,6px) rotate(-45deg)` : `translate(0px,0px) rotate(0deg)` }}></div>
         <div className='w-5 shrink-0 h-[2px] bg-black duration-300 origin-center ' style={{ transform: (isMenuOpen) ? `scaleX(0)` : `scaleX(1)` }}></div>
         <div className='w-5 shrink-0 h-[2px] bg-black duration-200' style={{ transform: (isMenuOpen) ? `translate(0px,-6px) rotate(45deg)` : `translate(0px,0px) rotate(0deg)` }}></div>
       </div>
-
-
-      <ul className="second-menu fixed t-[50vh] bg-red-100 w-[250px] h-[250px]">
-        <div className="back">
-          <Image
-           
-          />
-        </div>
-        <li>Home</li>
-        <li>About</li>
-        <li>Projects</li>
-        <li>Contact</li>
-      </ul>
 
 
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundColor:{
+        "primary":'var(--primary-background)',
+        "secondary":'var(--secondary-background)'
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
