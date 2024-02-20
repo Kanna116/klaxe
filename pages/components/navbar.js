d8cd8b668a83ad030801b0d819d0ac8a1de0d808
Kanna116 <muralikanna77@gmail.com>
2024-02-20 20:18:35 +0530

    navbar is now responsive

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -4,10 +4,11 @@ import Image from 'next/image';
 
 function Navbar() {
 
-  const [isMenuOpen, SetIsMenuOpen] = useState(true);
+  const [isMenuOpen, SetIsMenuOpen] = useState(false);
+
   return (
     <div className='w-full h-[50px] p-[10px] fixed top-[10px] left-0 lg:px-[96px] md:px-[64px] px-[24px] flex items-center justify-between'>
-      <div className='h-full w-fit'>
+      <div className='h-full w-fit shrink-0'>
         <Logo />
       </div>
       <ul className='lg:flex md:flex items-center gap-5 bg-zinc-100 w-fit text-black px-5 py-1 rounded text-xs hidden'>
@@ -28,7 +29,7 @@ function Navbar() {
       </div>
 
       <ul
-        className={`fixed top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2  w-[300px] h-[300px] rounded-full flex items-center justify-center font-secondary flex-col gap-2 bg-secondary text-secondary duration-300 origin-center  ${isMenuOpen ? 'scale-100' : 'scale-0'}`}>
+        className={`fixed top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2  w-[300px] h-[300px] rounded-full flex items-center justify-center font-secondary flex-col gap-2 bg-secondary text-secondary duration-300 origin-center  ${isMenuOpen ? 'scale-100' : 'scale-0'} lg:hidden md:hidden `}>
         <li className='text-lg font-semibold uppercase'>Home</li>
         <li className='text-lg font-semibold uppercase'>About</li>
         <li className='text-lg font-semibold uppercase'>Projects</li>
