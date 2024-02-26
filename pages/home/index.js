3c13562e1f5b768b95fea4b51d71b137a5664035
Kanna116 <muralikanna77@gmail.com>
2024-02-26 21:04:48 +0530

    hero section component is made

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/navbar'
+import HeroSection from '../components/herosection'
 
 const HomePage = () => {
   return (
     <div className='px-[24px] md:px-[64px] lg:px-[96px] min-h-screen w-full'>
         <Navbar />
+        <HeroSection />
     </div>
   )
 }
