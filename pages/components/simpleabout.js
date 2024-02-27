373ee36b3999cc314eb7764486525896f992c06b
Kanna116 <muralikanna77@gmail.com>
2024-02-27 08:38:44 +0530

    simple about text added and button created

--- pages/components/simpleabout.js
+++ pages/components/simpleabout.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { simpleAbout } from '../constants'
 
 function SimpleAbout() {
-  return (
-    <div>SimpleAbout</div>
-  )
+    return (
+        <div className='w-full flex items-center justify-center flex-col gap-5 py-20'>
+            <p className='text-3xl font-medium w-4/5'>{simpleAbout}</p>
+            <button className='lg:px-[50px] px-[20px] lg:py-[15px] py-[10px] bg-white text-black text-xs lg:text-xl mt-5'>More</button>
+        </div>
+    )
 }
 
 export default SimpleAbout
