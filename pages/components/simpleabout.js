f70d7e74a373432eabe91a0a2c8222fc72670a8b
Kanna116 <muralikanna77@gmail.com>
2024-02-27 08:44:28 +0530

    constant updated

--- pages/components/simpleabout.js
+++ pages/components/simpleabout.js
@@ -4,7 +4,7 @@ import { simpleAbout } from '../constants'
 function SimpleAbout() {
     return (
         <div className='w-full flex items-center justify-center flex-col gap-5 py-20'>
-            <p className='text-3xl font-medium w-4/5'>{simpleAbout}</p>
+            <p className='lg:text-3xl text-xl lg:font-medium font-normal lg:w-4/5 w-full'>{simpleAbout.text}</p>
             <button className='lg:px-[50px] px-[20px] lg:py-[15px] py-[10px] bg-white text-black text-xs lg:text-xl mt-5'>More</button>
         </div>
     )
--- pages/constants/index.js
+++ pages/constants/index.js
@@ -24,7 +24,10 @@ export const heroText = [
     "Code with precision design with passion"
 ]
 
-export const simpleAbout = `I'm Murali Krishna Sake, a self-taught UX designer and front-end developer. I specialize in solving everyday problems through innovative and modern design trends. My approach involves empowering today's digital experiences with cross-functional development, ensuring a seamless user journey and elevating brand value. From ideation to deployment, I take pride in bringing creativity and functionality together. Explore my portfolio to witness how I transform ideas into impactful designs.`
+export const simpleAbout = {
+    text:`I'm Murali Krishna Sake, a self-taught UX designer and front-end developer. I specialize in solving everyday problems through innovative and modern design trends. My approach involves empowering today's digital experiences with cross-functional development, ensuring a seamless user journey and elevating brand value. From ideation to deployment, I take pride in bringing creativity and functionality together. Explore my portfolio to witness how I transform ideas into impactful designs.`,
+    link:''
+}
 
 
 
