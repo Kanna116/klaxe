a2788a18d45ac88d41b69443dc124e60dbd5afb7
Kanna116 <muralikanna77@gmail.com>
2024-02-27 09:52:09 +0530

    mouse move animation made

--- pages/components/quicksociallinks.js
+++ pages/components/quicksociallinks.js
@@ -1,19 +1,43 @@
-import { Children } from "react";
+import { useState } from "react";
 import { FaCodepen, FaGithub, FaGlobeAmericas, FaLinkedinIn } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
 const QuickSocialLinks = () => {
 
-    return (
-        <div className="quick-social-icons absolute left-0 bottom-10 h-fit w-fit lg:flex flex-col hidden">
-            <div
-                onMouseMove={(dets) => {
+    const [transformStyle, setTransformStyle] = useState({});
+    const handleMouseMove = (e) => {
+        const rect = e.currentTarget.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        // const translateX = x - rect.width / 2;
+        // const translateY = y - rect.height / 2;
 
-                }}
-                className="w-fit h-fit p-4 icon-container bg-zinc-500">
+        const translateX = Math.min(x - rect.width / 2, 20); 
+        const translateY = Math.min(y - rect.height / 2, 20);
+
+        const transform = `translate(${translateX}px, ${translateY}px)`;
+        setTransformStyle({ transform });
+    }
+
+    const handleMouseLeave = () => {
+        const transform = `translate(0px, 0px)`;
+        setTransformStyle({ transform });
+    }
+
+    return (
+        <div className="quick-social-icons absolute left-0 bottom-10 h-fit w-fit lg:flex md:flex flex-col hidden">
+            <div
+                onMouseMove={(e) => handleMouseMove(e)}
+                onMouseLeave={handleMouseLeave}
+                className="w-fit h-fit p-4 icon-container bg-zinc-500 flex items-center justify-center">
                 <span
-                    className="text-2xl icon"><FaLinkedinIn /></span>
+                    style={transformStyle}
+                    className="text-2xl icon pointer-events-none">
+                    <FaLinkedinIn />
+                </span>
             </div>
+
+            
             <div className="w-fit h-fit p-1 ">
                 <span className="text-2xl"><FaGithub /></span>
             </div>
