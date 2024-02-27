0550ead8a3a3ca27bc4c8c760a017b0bc254a125
Kanna116 <muralikanna77@gmail.com>
2024-02-27 16:42:09 +0530

    tech stack bar made

--- pages/components/techstackbar.js
+++ pages/components/techstackbar.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { techStackIcons } from '../constants'
 
 function TechStackbar() {
-  return (
-    <div>TechStackbar</div>
-  )
+    return (
+        <div className='w-full h-fit py-5 px-3 flex items-center mb-3 justify-between border-[#fffffff] border-y-[1px]'>
+            {
+                techStackIcons.map((item, index) => <span
+                 key={index}
+                 className='text-2xl'
+                 >{item}</span>)
+            }
+        </div>
+    )
 }
 
 export default TechStackbar
