8492ca19295f8f7855a6dd5c5dd8339bd70afd7a
Kanna116 <muralikanna77@gmail.com>
2024-02-27 08:20:56 +0530

    build error removed

--- pages/components/herosection.js
+++ pages/components/herosection.js
@@ -10,7 +10,7 @@ const HeroSection = () => {
                 <h4 className='font-semibold text-[#ffffff] text-xs lg:text-3xl'>{heroText[0]}</h4>
                 <h1 className='lg:text-6xl md:text-4xl text-xl font-bold capitalize'>{heroText[1]}</h1>
                 <h1 className='lg:text-5xl md:text-3xl text-xl font-semibold capitalize'>{heroText[2]}</h1>
-                <h4 className='font-semibold text-[#ffffff] text-xs lg:text-xl'>"{heroText[3]}"</h4>
+                <h4 className='font-semibold text-[#ffffff] text-xs lg:text-xl'>{heroText[3]}</h4>
                 <button className='lg:px-[50px] px-[20px] lg:py-[15px] py-[10px] bg-white text-black text-xs lg:text-xl mt-5'>Connect</button>
             </div>
             <div className="back absolute top-0 left-1/2 -translate-x-1/2 h-screen w-screen -z-[1] overflow-hidden">
