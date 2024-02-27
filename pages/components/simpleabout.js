119bfce87d0484de3fdfc3e98d70f6858fac3d86
Kanna116 <muralikanna77@gmail.com>
2024-02-27 08:47:42 +0530

    buton updated

--- pages/components/simpleabout.js
+++ pages/components/simpleabout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { simpleAbout } from '../constants'
+import { FiArrowUpRight } from "react-icons/fi";
 
 function SimpleAbout() {
     return (
         <div className='w-full flex items-center justify-center flex-col gap-5 py-20'>
             <p className='lg:text-3xl text-xl lg:font-medium font-normal lg:w-4/5 w-full'>{simpleAbout.text}</p>
-            <button className='lg:px-[50px] px-[20px] lg:py-[15px] py-[10px] bg-white text-black text-xs lg:text-xl mt-5'>More</button>
+            <button className='lg:px-[50px] px-[20px] lg:py-[15px] py-[10px] bg-white text-black text-xs lg:text-xl mt-5 flex gap-2 items-center'>More <span className='text-2xl'><FiArrowUpRight /></span></button>
         </div>
     )
 }
