fe197cd776804a79c472ba8b49c11823a60e5552
Kanna116 <muralikanna77@gmail.com>
2024-02-28 16:08:16 +0530

    removed a warning in connect section

--- pages/components/connect.js
+++ pages/components/connect.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Connect = () => {
     return (
         <div className='h-screen w-full flex items-center justify-center gap-10 flex-col'>
-            <h1 className='text-6xl'>Let's Work Together</h1>
+            <h1 className='text-6xl'>Let&apos;s Work Together</h1>
             <div className='flex gap-5 items-center'>
                 <button className='text-black bg-white px-[30px] py-[10px] rounded-[3px] text-base font-semibold'>Connect</button>
                 <button className='text-white border-white border-[1px] px-[30px] py-[10px] rounded-[3px] text-base font-semibold'>Mail</button>
