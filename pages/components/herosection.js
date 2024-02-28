735c22ae27fa797e7ee40af610a9db3a80fe0d9b
Kanna116 <muralikanna77@gmail.com>
2024-02-28 16:37:28 +0530

    hero section updated

--- pages/components/herosection.js
+++ pages/components/herosection.js
@@ -11,10 +11,10 @@ const HeroSection = () => {
         <div className='w-full h-screen relative'>
             <div className="front w-full h-full pt-[60px] flex flex-col items-center justify-center lg:gap-6 gap-2">
                 <h4 className='font-semibold text-[#ffffff] text-xs lg:text-3xl'>{heroText[0]}</h4>
-                <h1 className='lg:text-6xl md:text-4xl text-white text-xl font-bold capitalize'>{heroText[1]}</h1>
-                <h1 className='lg:text-5xl md:text-3xl text-white text-xl font-semibold capitalize'>{heroText[2]}</h1>
+                <h1 className='lg:text-6xl md:text-4xl text-white text-2xl font-bold capitalize'>{heroText[1]}</h1>
+                <h1 className='lg:text-5xl md:text-3xl text-white text-2xl font-semibold capitalize'>{heroText[2]}</h1>
                 <h4 className='font-semibold text-[#ffffff] text-xs lg:text-xl'>{heroText[3]}</h4>
-                <button className='lg:px-[50px] px-[20px] lg:py-[15px] py-[10px] bg-white text-black text-xs lg:text-xl mt-5'>Connect</button>
+                <button className='lg:px-[30px] px-[20px] lg:py-[10px] py-[10px] rounded-[3px] bg-white text-black font-semibold text-xs lg:text-xl mt-5'>Connect</button>
             
             {/* scroll down suggestion */}
             <div className="scroll-down absolute bottom-10 left-1/2 -translate-x-1/2 flex flex-col gap-3">
