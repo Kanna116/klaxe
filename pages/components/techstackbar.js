98904b42482a0998fe2ef66abd1fa20a8c2743de
Kanna116 <muralikanna77@gmail.com>
2024-02-28 16:32:03 +0530

    techstackbar responsive done

--- pages/components/techstackbar.js
+++ pages/components/techstackbar.js
@@ -3,7 +3,7 @@ import { techStackIcons } from '../constants'
 
 function TechStackbar() {
     return (
-        <div className='w-full h-fit py-5 px-3 flex items-center mb-3 justify-between border-[#fffffff] border-y-[1px]'>
+        <div className='w-full h-fit py-5 px-3 flex items-center mb-3 flex-wrap gap-[10%] gap-y-3 lg:gap-[0%] lg:gap-y-0 justify-between border-[#fffffff] border-y-[1px]'>
             {
                 techStackIcons.map((item, index) => <span
                     key={index}
