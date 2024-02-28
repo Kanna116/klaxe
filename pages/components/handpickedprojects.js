46631b9d11e5b9d61d7e037d181a082fc01837f5
Kanna116 <muralikanna77@gmail.com>
2024-02-28 09:15:20 +0530

    updated the display of projects and made another component for card

--- pages/components/handpickedprojects.js
+++ pages/components/handpickedprojects.js
@@ -1,34 +1,21 @@
-import Image from 'next/image'
-import { handpickedProjects } from '../constants'
+import { useEffect, useState } from 'react';
+import { handpickedProjects } from '../constants';
+import HandPickedProjectCard from './handpickedprojectcard';
 
 const HandPickedProjects = () => {
+    const [randomLeft, setRandomLeft] = useState(0);
+
+    useEffect(() => {
+        // Calculate random left value once on component mount
+        const randomValue = Math.floor(Math.random() * 80);
+        setRandomLeft(randomValue);
+    }, []);
     return (
         <div className='w-full h-screen py-10 overflow-hidden'>
-            <h1 className='text-center text-4xl sticky top-0 z-[20] font-semibold'>Hand Picked Projects</h1>
-            <div className='w-full h-full mt-10 flex flex-col items-center justify-start lg:gap-[20vh] gap-10  overflow-y-scroll sticky top-0'>
+            <h1 className='text-center text-4xl z-[20] font-semibold'>Hand Picked Projects</h1>
+            <div className='w-full h-fit mt-10 flex flex-col lg:flex-row lg:flex-wrap items-center lg:items-start lg:justify-center justify-start lg:gap-[15vh] gap-10  overflow-y-scroll sticky top-0'>
                 {
-                    handpickedProjects.map((item, index) => {
-                        return (
-                            <div
-                                key={index}
-                                className='w-full h-fit relative shrink-0 flex items-center justify-center md:justify-start lg:justify-start'
-                            >
-                                <div
-                                    className='group h-[40vh] aspect-square shrink-0 relative overflow-hidden'>
-                                    <Image
-                                        src={item.image}
-                                        width={500}
-                                        height={500}
-                                        alt={`project image of ${item.title}`}
-                                        draggable={false}
-                                        className='w-full h-full object-cover object-center'
-                                    />
-                                    <div className='absolute inset-0  lg:group-hover:bg-[#000000aa] bg-gradient-to-t lg:from-transparent from-black duration-200 ease-in-out'></div>
-                                    <h1 className='absolute bottom-5 left-5 text-2xl font-semibold lg:translate-y-14 translate-y-0 group-hover:translate-y-0 duration-200 ease-in-out'>{item.title}</h1>
-                                </div>
-                            </div>
-                        )
-                    })
+                    handpickedProjects.map((item, index) => <HandPickedProjectCard key={index} item={item} />)
                 }
             </div>
         </div>
