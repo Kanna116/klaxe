5177fd9a4fd7449e83e79e09d1a17ae73c2fa1e6
Kanna116 <muralikanna77@gmail.com>
2024-02-28 16:38:44 +0530

    simple about button updated

--- pages/components/simpleabout.js
+++ pages/components/simpleabout.js
@@ -6,7 +6,7 @@ function SimpleAbout() {
     return (
         <div className='w-full flex items-center justify-center flex-col gap-5 py-20'>
             <p className='lg:text-3xl text-xl lg:font-medium font-normal lg:w-4/5 w-full'>{simpleAbout.text}</p>
-            <button className='lg:px-[50px] px-[20px] lg:py-[15px] py-[10px] bg-white text-black text-xs lg:text-xl mt-5 flex gap-2 items-center'>More <span className='text-2xl'><FiArrowUpRight /></span></button>
+            <button className='lg:px-[30px] px-[20px] lg:py-[10px] py-[10px] rounded-[3px] bg-white text-black font-semibold text-xs lg:text-xl mt-5 flex gap-2 items-center'>More <span className='text-2xl'><FiArrowUpRight /></span></button>
         </div>
     )
 }
