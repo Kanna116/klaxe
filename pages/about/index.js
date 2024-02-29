3fb2ffa7eae6651ec5dc3740d308fd442d82a077
Kanna116 <muralikanna77@gmail.com>
2024-02-29 10:43:43 +0530

    about page component created

---
+++ pages/about/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+
+const index = () => {
+    return (
+        <>
+            <Navbar />
+
+        </>
+    )
+}
+
+export default index
