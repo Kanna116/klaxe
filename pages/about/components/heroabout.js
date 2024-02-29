dc80c5b89ed3096987a4fc09d2e050b8fa7335c9
Kanna116 <muralikanna77@gmail.com>
2024-02-29 12:01:19 +0530

    hero about made resposive

--- pages/about/components/heroabout.js
+++ pages/about/components/heroabout.js
@@ -1,10 +1,11 @@
+import { heroAbout } from '@/pages/constants'
 import Image from 'next/image'
 import React from 'react'
 
 const HeroAbout = () => {
     return (
-        <div className='w-full h-[70vh] bg-zinc-600  mt-[60px] flex items-center justify-evenly'>
-            <div className='w-[30vw] aspect-square rounded-full overflow-hidden'>
+        <div className='w-full h-fit py-16 mt-[60px] flex flex-col lg:flex-row md:flex-row lg:items-center items-center lg:justify-between md:justify-between justify-start gap-5 lg:gap-20 md:gap-20'>
+            <div className='lg:w-[30vw] md:w-[30vw] w-[80vw] aspect-square rounded-full overflow-hidden shrink-0'>
                 <Image
                     src={"/images/about/hero-about-image.jpg"}
                     width={1000}
@@ -15,8 +16,10 @@ const HeroAbout = () => {
                 />
             </div>
 
-            <div className='w-1/2 h-full flex flex-col items-start gap-5'>
-                <h4></h4>
+            <div className='w-full  h-full flex flex-col items-start justify-between gap-5'>
+                <h1 className='text-3xl font-bold'>About Me</h1>
+                <h4 className='w-full lg:text-2xl md:text-xl text-base font-normal'>{heroAbout.desc}</h4>
+                <a href={heroAbout.link}><button>Support</button></a>
             </div>
 
         </div>
