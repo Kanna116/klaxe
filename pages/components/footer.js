4bc4fa5b5cdcad05becadf55aa2a834ec30c2d15
Kanna116 <muralikanna77@gmail.com>
2024-02-29 10:40:12 +0530

    footer z index fixed

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -5,7 +5,7 @@ import { socialIcons } from '../constants';
 const Footer = () => {
     let year = new Date().getFullYear()
     return (
-        <div className='py-5 gap-4 w-full border-t-[2px] border-white flex flex-col lg:flex-row md:flex-row items-center justify-between'>
+        <div className='py-5 gap-4 w-full border-t-[2px] border-white flex flex-col lg:flex-row md:flex-row items-center justify-between z-auto'>
             <div className='h-full flex flex-col lg:items-start md:items-start items-center justify-center gap-2'>
                 <h4 className='font-normal text-base lg:text-start md:text-start text-center'>Designed and Developed by Murali Krishna Sake</h4>
                 <p className='flex items-center gap-2 font-thin text-sm'><span><AiOutlineCopyright /></span>{year} all rights reserved</p>
@@ -15,7 +15,11 @@ const Footer = () => {
                 {
                     socialIcons.map((item, index) => {
                         return (
-                            <a key={index} href={item.link} target='_blank' className='hover:scale-[1.5] duration-150'>
+                            <a
+                                key={index}
+                                href={item.link}
+                                target='_blank'
+                                className='hover:scale-[1.5] duration-150'>
                                 <span className='text-2xl'>{item.icon}</span>
                             </a>
                         )
