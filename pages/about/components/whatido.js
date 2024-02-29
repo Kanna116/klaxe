dcb0564f8db31a0658d11aad0503a11fe3ba8f34
Kanna116 <muralikanna77@gmail.com>
2024-02-29 17:51:00 +0530

    chips toggling updated

--- pages/about/components/whatido.js
+++ pages/about/components/whatido.js
@@ -5,16 +5,19 @@ import WhatIDoChip from './whatidochip';
 const WhatIdo = () => {
     const [chips, setChips] = useState(whatIDoChips);
 
+    const handleChipOpen = (id) => {
+        setChips(prevValues => prevValues.map(item => item.id === id ? { ...item, isOpen: !item.isOpen } : { ...item }))
+    }
+
     return (
         <div className='w-full h-fit flex items-start justify-between lg:flex-row flex-col'>
             <h1 className='text-5xl font-bold'>What I Do ?</h1>
             <div className='w-1/2 h-fit min-h-[60vh] flex items-start justify-start flex-col gap-3 origin-top'>
-                {/* <WhatIDoChip />
-                <WhatIDoChip />
-                <WhatIDoChip /> */}
+
                 {
-                    chips.map((item, index)=> <WhatIDoChip key={index} {...item} />)
+                    chips.map((item) => <WhatIDoChip key={item.id} {...item} handleChipOpen={handleChipOpen} />)
                 }
+
             </div>
         </div>
     )
--- pages/about/components/whatidochip.js
+++ pages/about/components/whatidochip.js
@@ -3,8 +3,9 @@ import { BsPlusLg } from "react-icons/bs";
 
 
 const WhatIDoChip = (props) => {
+    const { id, title, desc, isOpen, handleChipOpen } = props
 
-    
+
     return (
         <div
             style={{ backgroundColor: isOpen ? 'white' : 'black' }}
@@ -12,15 +13,13 @@ const WhatIDoChip = (props) => {
         >
             <div className='flex items-center justify-between px-5 h-[100px] duration-300'
                 style={{ paddingLeft: isOpen ? "40px" : "20px" }}
-            // onClick={() => setisOpen(prevValue => !prevValue)}
+                onClick={() => handleChipOpen(id)}
             >
-                <h2 className='text-3xl mix-blend-difference'>Web Development</h2>
+                <h2 className='text-3xl mix-blend-difference'>{title}</h2>
                 <span className='text-3xl mix-blend-difference duration-300' style={{ rotate: isOpen ? "135deg" : "0deg" }}><BsPlusLg /></span>
             </div>
             {
-                isOpen && <p className='animateChippara w-full h-fit pl-20 pr-5 py-5 mix-blend-difference duration-500'>Transforming great UI's into code from the past year. With immense concentration and pixel perfection weaved into every product.
-                    Transforming modern designs into actual working projects through mastery in code
-                </p>
+                isOpen && <p className='animateChippara w-full h-fit pl-20 pr-5 py-5 mix-blend-difference duration-500'>{desc}</p>
             }
 
         </div>
