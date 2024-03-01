b56d1ca189de1217a00b2b3dd44d03676a0d8c69
Kanna116 <muralikanna77@gmail.com>
2024-03-01 12:23:09 +0530

    my pics section made responsive

--- pages/about/components/mypics.js
+++ pages/about/components/mypics.js
@@ -4,18 +4,18 @@ import React from 'react'
 
 const MyPics = () => {
     return (
-        <div className='w-full h-screen flex flex-wrap items-start justify-start gap-0'>
+        <div className='w-full lg:h-screen md:h-screen h-fit flex flex-wrap items-start justify-start lg:gap-0 md:gap-0 gap-3 lg:flex-row md:flex-row flex-col my-10'>
             {
                 myPics.map((item, index) => {
                     return (
-                        <div className='w-1/3 h-1/2 shrink-0 flex items-center justify-center p-20'>
+                        <div className='lg:w-1/3 md:w-1/3 w-full lg:h-1/2 md:h-1/2 h-fit shrink-0 flex items-center justify-center lg:p-14 md:p-6 p-5 '>
                             <Image
-                            src={item.image}
-                            width={500}
-                            height={500}
-                            alt={`Murali Krishna photo ${item.id}`}
-                            draggable={false}
-                            className='w-full h-full object-contain object-center'
+                                src={item.image}
+                                width={500}
+                                height={500}
+                                alt={`Murali Krishna photo ${item.id}`}
+                                draggable={false}
+                                className='w-full h-full object-contain object-center'
                             />
                         </div>
                     )
