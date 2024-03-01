08187186b7eb7a5db4c166bbef22949b8e2271f9
Kanna116 <muralikanna77@gmail.com>
2024-03-01 21:29:09 +0530

    sample data of project detail is added

---
+++ pages/projects/projectdata.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const projectdata = [
+    {
+        id: 1,
+        title: `Velvet Vista`,
+        desc: `It is an Ecommerce site for the most fine international qualitied jewellary  starting from silver to diamonds. This is a multi-page site made based on next js framework and react js as library. This has been enhanced in many ways to represent the quality and prestige a consumer get through the jewellary.
+                Made with keen eye towards style and brand empowering in the all the ways possible. Constantly striving for the enhancement of royalty`,
+        liveLink: ``,
+        codeLink: ``,
+        heroImage: ``,
+        details: [
+            {
+                heading: `Type`,
+                information: [`Web development`, `Ecommerce`]
+
+            },
+            {
+                heading: `Tools Used`,
+                information: [`Next js`, `React js`, `Tailwind CSS`, `JSX`, `Node js`]
+            },
+            {
+                heading: `Year Made`,
+                information: [`2023`]
+            },
+            {
+                heading: `Role`,
+                information: [`Front End Developer`]
+            },
+            {
+                heading: `Product`,
+                information: [`Ecommerce jewellary`]
+            },
+        ],
+        problemSolved: ``,
+        demoVideo: ``,
+        collection: [
+            ``, ``
+        ],
+
+
+    }
+]
+
+
+export default projectdata
