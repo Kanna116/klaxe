398c559eb5e8891c0f00bf67bf4705e4ac2f2a9b
Kanna116 <muralikanna77@gmail.com>
2024-03-01 22:21:46 +0530

    project display based on url

--- pages/projects/[projectdetail].js
+++ pages/projects/[projectdetail].js
@@ -1,16 +1,45 @@
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/navbar';
+import projectdata from './projectdata';
 
 const ProjectDetail = () => {
 
     const router = useRouter();
     const { projectdetail } = router.query
 
+
+    /*
+    
+    this data is temporary 
+    used to check the layout and how the project will be displayed
+    later the data will come from the projects page
+
+    */
+
+    if (!projectdetail) {
+        return <div className='w-screen min-h-screen flex items-center justify-center'>Loading...</div>;
+    }
+
+
+    const [projectsData, setProjectsData] = useState(projectdata);
+
+    const project = projectsData.find(item => item.url === projectdetail)
+    
+    if (!project) {
+        return <div className='w-screen min-h-screen flex items-center justify-center bg-red-400'>Loading...</div>;
+    }
+
+    const { title, desc, liveLink, codeLink, heroImage, details, problemSolved, demoVideo, collection } = project
+
+
+
     return (
-        <div className='w-full min-h-screen mt-[60px]'>
+        <div className='px-[24px] md:px-[64px] lg:px-[96px] min-h-screen w-full mt-[60px]'>
             <Navbar />
-            sdflkndslk
+            <h1 className='text-5xl font-semibold'>{title}</h1>
+            <p className='text-xs'>{desc}</p>
+            <button></button>
         </div>
     )
 }
--- pages/projects/projectdata.js
+++ pages/projects/projectdata.js
@@ -38,6 +38,8 @@ const projectdata = [
             ``, ``
         ],
 
+        url:'velvet-vista'
+
 
     }
 ]
