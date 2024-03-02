77a7adfe4a8da72215b260a252772f8c32722c4d
Kanna116 <muralikanna77@gmail.com>
2024-03-02 15:43:58 +0530

    another project added

--- pages/projects/projectdata.js
+++ pages/projects/projectdata.js
@@ -44,86 +44,45 @@ const projectdata = [
     },
     {
         id: 1,
-        title: `Velvet Vista`,
-        desc: `It is an Ecommerce site for the most fine international qualitied jewellary  starting from silver to diamonds. This is a multi-page site made based on next js framework and react js as library. This has been enhanced in many ways to represent the quality and prestige a consumer get through the jewellary.
-                Made with keen eye towards style and brand empowering in the all the ways possible. Constantly striving for the enhancement of royalty`,
-        liveLink: `https://velvet-vista-klax.vercel.app/`,
-        codeLink: `https://github.com/Kanna116/velvet-vista`,
-        heroImage: `/images/projects/velvet-vista/hero-image.png`,
+        title: `Klick It`,
+        desc: `It is mobile app design which allows the user buy the products from nearby store. It can be either home delivery or Take away this also also allow the user to prebook the things they want from a store and skip the very long line in the store`,
+        liveLink: ``,
+        codeLink: ``,
+        heroImage: `/images/projects/Klick-it/hero-image.png`,
         details: [
             {
                 heading: `Type`,
-                information: [`Web development`, `Ecommerce`]
+                information: [`UX Design`, `Ecommerce`]
             },
             {
-                heading: `Tools Used`,
-                information: [`Next js`, `React js`, `Tailwind CSS`, `JSX`, `Node js`]
-            },
-            {
-                heading: `Role`,
-                information: [`Front End Developer`]
-            },
-            {
-                heading: `Year Made`,
-                information: [`2023`]
-            },
-            {
-                heading: `Product`,
-                information: [`Ecommerce jewellary`]
-            },
-        ],
-        problemSolved: `Made an Ecommerce site which allows the user enjoy the excellence the joy and pride in buying or wearing our  jewellary. Made with pixel perfection. Focused mainly on building the user satisfaction along with increasing the brand power and prestige. `,
-        demoVideo: `/videos/projects/velvet-vista-demo-video.webm`,
-        collection: [
-            `/images/projects/velvet-vista/collection-image-1.png`,
-            `/images/projects/velvet-vista/collection-image-2.png`,
-            `/images/projects/velvet-vista/collection-image-3.png`,
-
-        ],
-
-        url: 'velvet-vista'
-    },
-    {
-        id: 2,
-        title: `Velvet Vista`,
-        desc: `It is an Ecommerce site for the most fine international qualitied jewellary  starting from silver to diamonds. This is a multi-page site made based on next js framework and react js as library. This has been enhanced in many ways to represent the quality and prestige a consumer get through the jewellary.
-                Made with keen eye towards style and brand empowering in the all the ways possible. Constantly striving for the enhancement of royalty`,
-        liveLink: `https://velvet-vista-klax.vercel.app/`,
-        codeLink: `https://github.com/Kanna116/velvet-vista`,
-        heroImage: `/images/projects/velvet-vista/hero-image.png`,
-        details: [
-            {
-                heading: `Type`,
-                information: [`Web development`, `Ecommerce`]
-            },
-            {
-                heading: `Tools Used`,
-                information: [`Next js`, `React js`, `Tailwind CSS`, `JSX`, `Node js`]
+                heading: `Skills Used`,
+                information: [`Figma`, `UX Design`, `Prototyping`, `Wire framing`, `UX Research`]
             },
             {
                 heading: `Role`,
-                information: [`Front End Developer`]
+                information: [`UX designer`, `UX Researcher`]
             },
             {
                 heading: `Year Made`,
-                information: [`2023`]
+                information: [`2022`]
             },
             {
                 heading: `Product`,
-                information: [`Ecommerce jewellary`]
+                information: [`Store Ecommerce App`]
             },
         ],
-        problemSolved: `Made an Ecommerce site which allows the user enjoy the excellence the joy and pride in buying or wearing our  jewellary. Made with pixel perfection. Focused mainly on building the user satisfaction along with increasing the brand power and prestige. `,
-        demoVideo: `/videos/projects/velvet-vista-demo-video.webm`,
+        problemSolved: `Provided the functionality to the user to get the products either by getting them to delivered to home or Give them the opportunity to explore nearby shops and their products and get the delivery done as soon as possible. And provide seamless user journey `,
+        demoVideo: ``,
         collection: [
-            `/images/projects/velvet-vista/collection-image-1.png`,
-            `/images/projects/velvet-vista/collection-image-2.png`,
-            `/images/projects/velvet-vista/collection-image-3.png`,
+            `/images/projects/Klick-it/collection-image-1.png`,
+            `/images/projects/Klick-it/collection-image-2.png`,
+            `/images/projects/Klick-it/collection-image-3.png`,
 
         ],
 
-        url: 'velvet-vista'
+        url: 'klick-it'
     },
+
 ]
 
 
