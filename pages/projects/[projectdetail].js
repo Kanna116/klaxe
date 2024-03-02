ff557d8eef9dc9e4c5680bdc948a1f2965ed1fe4
Kanna116 <muralikanna77@gmail.com>
2024-03-02 08:00:08 +0530

    rendering, hooks bug removed and project display component  created
    in order to make the things clear I made the display in a different component

--- pages/projects/[projectdetail].js
+++ pages/projects/[projectdetail].js
@@ -1,15 +1,7 @@
 import { useRouter } from 'next/router';
-import { useState } from 'react';
-import Footer from '../components/footer';
-import Navbar from '../components/navbar';
-import Connect from './components/connect';
-import DemoVideo from './components/demovideo';
-import ImageCollection from './components/imagecollection';
-import ProblemSolved from './components/problemsolved';
-import ProjectHero from './components/projecthero';
-import ProjectInfo from './components/projectinfo';
-import ProjectOpinion from './components/projectopinion';
+import { useEffect, useState } from 'react';
 import projectdata from './projectdata';
+import ProjectDisplay from './projectdisplay';
 
 const ProjectDetail = () => {
 
@@ -25,34 +17,32 @@ const ProjectDetail = () => {
 
     */
 
-    if (!projectdetail) {
+    const [projectsData, setProjectsData] = useState(null); // Initialize projectsData with null
+
+    useEffect(() => {
+        // Fetch projectsData once projectdetail is defined
+        if (projectdetail) {
+            setProjectsData(projectdata);
+        }
+    }, [projectdetail]); // useEffect dependency
+
+
+    if (!projectdetail || !projectsData) {
         return <div className='w-screen min-h-screen flex items-center justify-center'>Loading...</div>;
     }
-
-    const [projectsData, setProjectsData] = useState(projectdata);
-
 
     const project = projectsData.find(item => item.url === projectdetail)
 
     if (!project) {
-        return <div className='w-screen min-h-screen flex items-center justify-center bg-red-400'>Loading...</div>;
+        return <div className='w-screen min-h-screen flex items-center justify-center'>Sorry The page You are looking for is not made yet  <br /> :'(</div>;
     }
 
-    const { title, desc, liveLink, codeLink, heroImage, details, problemSolved, demoVideo, collection } = project
 
 
 
     return (
         <div className='px-[24px] md:px-[64px] lg:px-[96px] min-h-screen w-full mt-[60px]'>
-            <Navbar />
-            <ProjectHero {...project} />
-            <ProjectInfo details={details} />
-            <ProblemSolved problemSolved={problemSolved} />
-            <DemoVideo demoVideo={demoVideo} />
-            <ImageCollection collection={collection} title={title} />
-            <ProjectOpinion />
-            <Connect />
-            <Footer />
+            <ProjectDisplay {...project} />
         </div>
     )
 }
---
+++ pages/projects/projectdisplay.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Footer from '../components/footer';
+import Navbar from '../components/navbar';
+import Connect from './components/connect';
+import DemoVideo from './components/demovideo';
+import ImageCollection from './components/imagecollection';
+import ProblemSolved from './components/problemsolved';
+import ProjectHero from './components/projecthero';
+import ProjectInfo from './components/projectinfo';
+import ProjectOpinion from './components/projectopinion';
+
+const ProjectDisplay = (project) => {
+    const { title, desc, liveLink, codeLink, heroImage, details, problemSolved, demoVideo, collection } = project
+
+    return (
+        <>
+            <Navbar />
+            <ProjectHero {...project} />
+            <ProjectInfo details={details} />
+            <ProblemSolved problemSolved={problemSolved} />
+            <DemoVideo demoVideo={demoVideo} />
+            <ImageCollection collection={collection} title={title} />
+            <ProjectOpinion />
+            <Connect />
+            <Footer /></>
+    )
+}
+
+export default ProjectDisplay
