8eb67b6d000572b5039536185bc9ebffa97fb442
Kanna116 <muralikanna77@gmail.com>
2024-03-02 09:24:05 +0530

    projects page component created

---
+++ pages/projects/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div className='px-[24px] md:px-[64px] lg:px-[96px] min-h-screen w-full'>
+            asdansdkl
+        </div>
+    )
+}
+
+export default index
