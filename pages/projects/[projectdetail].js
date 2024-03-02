5c83acb03eac8acdd9cc329dff1f60e9697a97cd
Kanna116 <muralikanna77@gmail.com>
2024-03-02 07:14:15 +0530

    updated title section in project detail page

--- pages/projects/[projectdetail].js
+++ pages/projects/[projectdetail].js
@@ -41,15 +41,19 @@ const ProjectDetail = () => {
     return (
         <div className='px-[24px] md:px-[64px] lg:px-[96px] min-h-screen w-full mt-[60px]'>
             <Navbar />
-            <h1 className='text-5xl font-semibold'>{title}</h1>
-            <p className='text-xs'>{desc}</p>
-            <div>
-                <a href={liveLink} target='_blank'>
-                    <button className='px-[40px] py-[15px] bg-white mix-blend-difference rounded-special text-black font-semibold text-lg flex items-center gap-3'>View <span className='text-2xl'><FiArrowUpRight /></span></button>
-                </a>
-                <a href={codeLink} target='_blank'>
-                    <button className='px-[40px] py-[15px]  mix-blend-difference rounded-special  font-semibold text-lg border-2'>Code</button>
-                </a>
+            <div className='w-full h-fit flex py-20'>
+                <div className='w-full flex gap-5'>
+                    <h1 className='text-5xl font-semibold w-1/2'>{title}</h1>
+                    <p className='text-sm w-1/2 font-extralight'>{desc}</p>
+                </div>
+                <div className='w-fit h-full px-10 flex flex-col items-start justify-start gap-5'>
+                    <a href={liveLink} target='_blank'>
+                        <button className='px-[40px] py-[15px] bg-white mix-blend-difference rounded-special text-black font-semibold text-lg flex items-center gap-3'>View <span className='text-2xl'><FiArrowUpRight /></span></button>
+                    </a>
+                    <a href={codeLink} target='_blank'>
+                        <button className='px-[40px] py-[15px]  mix-blend-difference rounded-special  font-semibold text-lg border-2'>Code</button>
+                    </a>
+                </div>
             </div>
 
             <div className='hero-image w-[80vw] aspect-video rounded-special mx-auto'>
