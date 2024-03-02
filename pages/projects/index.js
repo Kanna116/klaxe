22ea3d8b35b5062cfbfcd0f1e41c6158ca9e56b2
Kanna116 <muralikanna77@gmail.com>
2024-03-02 09:25:25 +0530

    basic components imported

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from '../components/navbar'
+import Connect from './components/connect'
+import Footer from '../components/footer'
 
 const index = () => {
     return (
         <div className='px-[24px] md:px-[64px] lg:px-[96px] min-h-screen w-full'>
-            asdansdkl
+            <Navbar />
+            <Connect />
+            <Footer />
         </div>
     )
 }
