2bbd7e761871fc7d39d834fe628d8adbd1e90beb
Kanna116 <muralikanna77@gmail.com>
2024-03-02 09:29:42 +0530

    heading created and one type of list type component created

---
+++ pages/projects/components/projectlisttype.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProjectListType = () => {
+  return (
+    <div>ProjectListType</div>
+  )
+}
+
+export default ProjectListType
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,13 +2,22 @@ import React from 'react'
 import Navbar from '../components/navbar'
 import Connect from './components/connect'
 import Footer from '../components/footer'
+import ProjectListType from './components/projectlisttype'
 
 const index = () => {
     return (
         <div className='px-[24px] md:px-[64px] lg:px-[96px] min-h-screen w-full'>
             <Navbar />
-            <Connect />
-            <Footer />
+            <div className='w-full h-fit py-10 flex flex-col items-center justify-center gap-5 mt-[60px]'>
+                <h1 className='text-5xl capitalize font-semibold'>Our Projects</h1>
+                <p className='text-2xl'>A place where you will find wonders.</p>
+            </div>
+
+            <ProjectListType />
+
+
+            {/* <Connect />
+            <Footer /> */}
         </div>
     )
 }
