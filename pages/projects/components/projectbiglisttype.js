1215386c3f56f9591383c16ae839cabea51c8520
Kanna116 <muralikanna77@gmail.com>
2024-03-04 13:38:09 +0530

    project big list type made responsive

--- pages/projects/components/projectbiglisttype.js
+++ pages/projects/components/projectbiglisttype.js
@@ -14,8 +14,8 @@ const ProjectBigListType = ({ id, title, desc, liveLink, codeLink, heroImage, de
 
 
     return (
-        <div className={`w-2/3 h-[50vh] flex gap-5 mx-auto mt-[50px] ${type ? "flex-row" : "flex-row-reverse"}`}>
-            <div className='h-full aspect-square bg-zinc-100 rounded-special overflow-hidden'>
+        <div className={`lg:w-2/3 md:w-full w-full lg:h-[50vh] md:h-[40vh] h-fit flex gap-5 mx-auto lg:mt-[50px] md:mt-[100px] mt-[50px] ${type ? "lg:flex-row" : "lg:flex-row-reverse"} ${type ? "md:flex-row" : "md:flex-row-reverse"} flex-col`}>
+            <div className='lg:h-full md:h-full h-fit lg:w-fit md:w-fit w-full  lg:aspect-square md:aspect-square aspect-video rounded-special overflow-hidden'>
                 <Image
                     src={collection[0]}
                     width={1000}
@@ -25,9 +25,9 @@ const ProjectBigListType = ({ id, title, desc, liveLink, codeLink, heroImage, de
                     className='w-full h-full object-cover object-center'
                 />
             </div>
-            <div className='w-1/2 h-full flex flex-col items-start justify-center gap-5 p-7'>
+            <div className='lg:w-1/2 md:w-1/2 w-full h-full flex flex-col items-start justify-center gap-5 lg:p-7 p-0'>
                 <h1 className='text-3xl font-semibold'>{title}</h1>
-                <p className='line-clamp-4 text-lg font-light'>{desc}</p>
+                <p className='line-clamp-4 lg:text-base text-xs lg:font-light font-thin'>{desc}</p>
                 <div className='flex items-center justify-start gap-[25px]'>
                     <Link href={`/projects/${url}`}>
                         <button className='px-[40px] py-[15px] bg-white mix-blend-difference rounded-special text-black font-semibold text-lg flex items-center gap-3'>
