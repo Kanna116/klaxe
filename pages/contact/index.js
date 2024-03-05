2ef1e4f2ac4b09df57af8aa9863ff8967f73cace
Kanna116 <muralikanna77@gmail.com>
2024-03-05 18:15:38 +0530

    modified the form

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -10,41 +10,80 @@ const Contact = () => {
         <div className='px-[24px] md:px-[64px] lg:px-[96px] min-h-screen w-full mt-[60px]'>
             <Navbar />
             <ContactHero />
-            <div className='w-full h-fit flex items-start justify-between'>
-                <div className="left w-1/2 h-fit [&>*]:w-full">
-                    <form
-                        onSubmit={e => e.preventDefault()}
-                        className='w-full h-full flex flex-col itemss-start justify-start'
-                    >
-                        <label htmlFor="name" className='flex items-center capitalize'>name <sup><PiStarFourFill /></sup></label>
-                        <input id='name' type="text" required className='text-black' />
+            <div className='w-full h-fit flex items-start justify-between min-h-screen'>
+                <form
+                    onSubmit={e => e.preventDefault()}
+                    className='w-1/2 h-full flex flex-col items-start justify-start [&>*]:w-full'
+                >
+                    <label
+                        htmlFor="name"
+                        className='flex items-center gap-2'>
+                        Name <sup><PiStarFourFill /></sup>
+                    </label>
+                    <input
+                        id='name'
+                        type="text"
+                        required
+                        className='text-black'
+                    />
 
-                        <label htmlFor="phone" className='flex items-center capitalize'>phone <sup><PiStarFourFill /></sup></label>
-                        <input id='phone' type="text" required className='text-black' />
+                    <label
+                        htmlFor="phone"
+                        className='flex items-center gap-2'>
+                        Phone <sup><PiStarFourFill /></sup>
+                    </label>
+                    <input
+                        id='phone'
+                        type="text"
+                        required
+                        className='text-black'
+                    />
 
-                        <label htmlFor="mail" className='flex items-center capitalize'>mail <sup><PiStarFourFill /></sup></label>
-                        <input id='mail' type="text" required className='text-black' />
+                    <label
+                        htmlFor="mail"
+                        className='flex items-center gap-2'>
+                        Mail <sup><PiStarFourFill /></sup>
+                    </label>
+                    <input
+                        id='mail'
+                        type="text"
+                        required
+                        className='text-black'
+                    />
 
-                        <label htmlFor="message" className='flex items-center capitalize'>Message <sup><PiStarFourFill /></sup></label>
-                        <textarea name="message" id="" cols="30" rows="10" className='text-black resize-y max-h-[350px] min-h-[40px] h-[150px]'></textarea>
-                        <h4 htmlFor="files">Upload Files <span>(if any)</span></h4>
-                        <div className='border-[1px] border-zinc-950 h-[70px]  flex items-center justify-between pr-4 gap-5'>
-                            <input
-                                type="file"
-                                name="file"
-                                id="file"
-                                className='hidden'
-                            />
-                            <label htmlFor="file" className="cursor-pointer shrink-0 bg-white text-black h-full w-1/2 grid place-items-center">
-                                Choose File
-                            </label>
-                            <p className='text-xs w-full'>No file Uploaded</p>
-                        </div>
-                        <button type='submit' className='flex items-center justify-center gap-3'>Submit <span><FiArrowUpRight /></span></button>
-                    </form>
-                </div>
-                <div className='w-1/4 h-fit'>
-                    sljdbnas
+                    <label
+                        htmlFor="message"
+                        className='flex items-center gap-2'>
+                        Message <sup><PiStarFourFill /></sup>
+                    </label>
+                    <textarea
+                        name="message"
+                        id=""
+                        cols="30" rows="10"
+                        className='text-black resize-y max-h-[350px] min-h-[40px] h-[150px]'>
+                    </textarea>
+                    <h4>Upload Files <span>(if any)</span></h4>
+                    <div className='border-[1px] border-zinc-950 h-[70px]  flex items-center justify-between pr-4 gap-5'>
+                        <input
+                            type="file"
+                            name="file"
+                            id="file"
+                            className='hidden'
+                        />
+                        <label htmlFor="file" className="cursor-pointer shrink-0 bg-white text-black h-full w-1/2 grid place-items-center">
+                            Choose File
+                        </label>
+                        <p className='text-xs w-full'>No file Uploaded</p>
+                    </div>
+                    <button
+                        type='submit'
+                        className='flex items-center justify-center gap-3'>
+                        Submit <span><FiArrowUpRight /></span>
+                    </button>
+                </form>
+                <div className='w-1/4 h-fit capitalize'>
+                    <h1 className='text-4xl font-light'>I would love to hear what you have in mind</h1>
+                    <h1 className='text-4xl font-light mt-10'>Let us not wast a minute</h1>
                 </div>
             </div>
 
