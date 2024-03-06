cc8afb755353c2ed105b5bf3b7da0940df6419d5
Kanna116 <muralikanna77@gmail.com>
2024-03-06 10:49:32 +0530

    Made contact page responsive

--- pages/contact/components/contacthero.js
+++ pages/contact/components/contacthero.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContactHero = () => {
     return (
-        <div className='w-full h-fit my-20 flex flex-col items-center justify-center gap-8'>
+        <div className='w-full h-fit my-20  flex flex-col items-center justify-center gap-8'>
             <h1 className='text-5xl font-semibold'>Contact</h1>
             <h4 className='text-xl'>Let us build something crafty</h4>
         </div>
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,13 +1,12 @@
+import { useState } from 'react';
 import { FiArrowUpRight } from 'react-icons/fi';
-import { PiStarFourFill } from "react-icons/pi";
+import { IoMdCheckmark } from 'react-icons/io';
 import Footer from '../components/footer';
 import Navbar from '../components/navbar';
+import ContactFiles from './components/contactfiles';
 import ContactHero from './components/contacthero';
 import ContactInput from './components/contactinput';
 import ContactMessage from './components/contactmessage';
-import ContactFiles from './components/contactfiles';
-import { useState } from 'react';
-import { IoMdCheckmark } from 'react-icons/io';
 
 const Contact = () => {
     const [formData, setFormData] = useState({
@@ -17,47 +16,42 @@ const Contact = () => {
         message: '',
         file: null
     });
+
+
     const [submitted, setSubmitted] = useState(false);
-
-
-
-
-    
 
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(formData)
-        setSubmitted(true)
 
-        // try {
-        //     const response = await fetch('https://formspree.io/f/mayrbyew', {
-        //         method: 'POST',
-        //         headers: {
-        //             'Content-Type': 'application/json',
-        //         },
-        //         body: JSON.stringify(formData),
-        //     });
+        try {
+            const response = await fetch('https://formspree.io/f/mayrbyew', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
 
-        //     if (response.ok) {
-        //         setSubmitted(true);
-        //         console.log('Form submitted successfully');
-        //     } else {
-        //         console.error('Failed to submit form');
-        //     }
-        // } catch (error) {
-        //     console.error('Error submitting form:', error);
-        // }
+            if (response.ok) {
+                setSubmitted(true);
+                console.log('Form submitted successfully');
+            } else {
+                console.error('Failed to submit form');
+            }
+        } catch (error) {
+            console.error('Error submitting form:', error);
+        }
     };
 
     return (
         <div className='px-[24px] md:px-[64px] lg:px-[96px] min-h-screen w-full mt-[60px]'>
             <Navbar />
             <ContactHero />
-            <div className='w-full h-fit flex items-start justify-between min-h-screen pb-10'>
+            <div className='w-full h-fit flex lg:flex-row flex-col-reverse items-start justify-between min-h-screen pb-10 '>
                 <form
                     onSubmit={e => handleSubmit(e)}
-                    className='w-1/2 h-full flex flex-col items-start justify-start'
+                    className='lg:w-1/2 w-full h-full flex flex-col items-start justify-start lg:mt-0 mt-5'
                 >
                     <ContactInput inputName='name' formData={formData} setFormData={setFormData} />
                     <ContactInput inputName='mail' formData={formData} setFormData={setFormData} />
@@ -67,18 +61,18 @@ const Contact = () => {
 
                     <button
                         type='submit'
-                        className='bg-white text-black w-fit flex px-[100px] py-[20px]  items-center justify-center gap-5 text-2xl font-semibold mt-10'>
+                        className='bg-white text-black w-fit flex lg:px-[100px] px-[50px] lg:py-[20px] py-[10px]  items-center mx-auto lg:mx-0 justify-center gap-5 lg:text-2xl text-xl font-semibold mt-10'>
                         Send <span className='text-xl'><FiArrowUpRight /></span>
                     </button>
 
-                    {submitted && <p className='text-primary text-xl font-light  mt-10 flex gap-5 items-center text-green-500'><span className='border-[1px] font-semibold p-2 rounded-full bg-green-400 text-black'><IoMdCheckmark /></span> Thank you for sending the details. I will connect with you ASAP</p>}
+                    {submitted && <p className='text-primary lg:text-xl text-sm font-light  mt-10 flex gap-5 items-center text-green-500'><span className='border-[1px] font-semibold p-2 rounded-full bg-green-400 text-black'><IoMdCheckmark /></span> Thank you for sending the details. I will connect with you ASAP</p>}
 
                 </form>
 
 
-                <div className='w-1/4 h-fit capitalize'>
-                    <h1 className='text-4xl font-light'>I would love to hear what you have in mind</h1>
-                    <h1 className='text-4xl font-light mt-10'>Let us not wast a minute</h1>
+                <div className='lg:w-1/4 w-full h-fit capitalize lg:mt-5 mt-2 border-[1px] lg:px-5 px-2 lg:py-20 py-5 text-center lg:text-start'>
+                    <h1 className='lg:text-4xl text-xl font-light'>I would love to hear what you have in mind</h1>
+                    <h1 className='lg:text-4xl text-xl font-light lg:mt-10 mt-3'>Let us not wast a minute</h1>
                 </div>
             </div>
 
