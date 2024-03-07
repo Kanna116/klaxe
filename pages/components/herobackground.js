9e5b37737bd24f37634829eaf99b63a4c2da8ef7
Kanna116 <muralikanna77@gmail.com>
2024-03-07 22:21:09 +0530

    made background responsive

--- pages/components/herobackground.js
+++ pages/components/herobackground.js
@@ -5,10 +5,13 @@ const HeroBackground = ({ interactiveValues }) => {
         <div
             className="hero-background absolute top-0 left-1/2 -translate-x-1/2 h-screen w-screen -z-[1] overflow-hidden flex items-center justify-center bg-[#000]">
             <div className="w-[75vmin] aspect-square rounded-full border-[1px] relative flex items-center justify-center">
-                <div className="interactive-mover absolute top-1/2 left-1/2 w-full aspect-[3/1] origin-center rounded-[50%] duration-300 ease-linear bg-[#ffffff10] border-[1px]"
+                <div className="absolute top-1/2 left-1/2 w-full aspect-[3/1] origin-center rounded-[50%] duration-700 ease-linear bg-[#ffffff30] border-[1px] md:block lg:block hidden"
                     style={{
                         transform: `translate(-50%,-50%) rotate(${interactiveValues.rotateValue}deg) scaleY(${interactiveValues.scaleYValue})`
                     }}
+                ></div>
+
+                <div className="interactive-mover md:hidden lg:hidden block absolute top-1/2 left-1/2 w-full aspect-[3/1] origin-center rounded-[50%] duration-700 ease-linear bg-[#ffffff30] border-[1px] -translate-x-1/2 -translate-y-1/2 rotate-[20deg] "
                 ></div>
             </div>
 
