8c7ce295805de3646558d1564caee660b01e7d75
Kanna116 <muralikanna77@gmail.com>
2024-03-07 22:23:35 +0530

    build problem resolved

--- pages/components/herobackground.js
+++ pages/components/herobackground.js
@@ -1,6 +1,10 @@
 
 const HeroBackground = ({ interactiveValues }) => {
 
+    if (!interactiveValues) {
+        return <div className='w-full h-full flex items-center justify-center'>Loading...</div>;
+    }
+
     return (
         <div
             className="hero-background absolute top-0 left-1/2 -translate-x-1/2 h-screen w-screen -z-[1] overflow-hidden flex items-center justify-center bg-[#000]">
