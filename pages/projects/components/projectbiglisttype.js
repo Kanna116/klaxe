598232c45bf0566e054c80b339209712127a5c35
Kanna116 <muralikanna77@gmail.com>
2024-03-07 16:01:13 +0530

    big list type modified

--- pages/projects/components/projectbiglisttype.js
+++ pages/projects/components/projectbiglisttype.js
@@ -11,14 +11,14 @@ const ProjectBigListType = ({ id }) => {
 
     // Don't access the props when it is undefined
     if (!project) {
-        return <div className='w-full h-screen flex items-center justify-center bg-zinc-700 text-white'>Loading...</div>; 
+        return <div className='w-full h-screen flex items-center justify-center bg-zinc-700 text-white'>Loading...</div>;
     }
 
     const { title, desc, codeLink, collection, url } = project
 
 
     return (
-        <div className={`lg:w-2/3 md:w-full w-full lg:h-[50vh] md:h-[40vh] h-fit flex gap-5 mx-auto lg:mt-[50px] md:mt-[100px] mt-[50px] lg:flex-row md:flex-row  flex-col`}>
+        <div className={`lg:w-2/3 md:w-full w-full lg:h-[50vh] md:h-[40vh] h-fit flex justify-between gap-5 mx-auto lg:mt-[50px] md:mt-[100px] mt-[50px]   flex-col  ${id % 2 === 0 ? `lg:flex-row md:flex-row` : `lg:flex-row-reverse md:flex-row-reverse`}`}>
             <div className='lg:h-full md:h-full h-fit lg:w-fit md:w-fit w-full  lg:aspect-square md:aspect-square aspect-video rounded-special overflow-hidden'>
                 <Image
                     src={collection[0]}
