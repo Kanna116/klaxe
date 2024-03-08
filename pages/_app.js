1a526218d119ac887b59547ae996dae1e9cb5d42
Kanna116 <muralikanna77@gmail.com>
2024-03-08 15:12:26 +0530

    modified app.js and given theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
 import "@/styles/globals.css";
 import ProjectState from "./context/projectstate";
+import { ThemeProvider } from 'next-themes'
 
 export default function App({ Component, pageProps }) {
 
   return (
     <>
-      <ProjectState>
-        <Component {...pageProps} />
-      </ProjectState>
+    {/* theme provider */}
+      <ThemeProvider>
+        {/* project data provider */}
+        <ProjectState>
+          <Component {...pageProps} />
+        </ProjectState>
+      </ThemeProvider>
     </>
   );
 }
