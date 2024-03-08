e90f93430cbb8f72f7160a84206286ea3a2ec90e
Kanna116 <muralikanna77@gmail.com>
2024-03-08 13:45:15 +0530

    handpickedprojects theme updated

--- pages/components/handpickedprojects.js
+++ pages/components/handpickedprojects.js
@@ -10,7 +10,7 @@ const HandPickedProjects = () => {
 
     return (
         <div className='w-full min-h-screen py-10 overflow-hidden'>
-            <h1 className='text-center text-4xl z-[20] font-semibold'>Hand Picked Projects</h1>
+            <h1 className='text-center text-4xl z-[20] font-semibold text-primary'>Hand Picked Projects</h1>
             <div className='w-full h-fit mt-10 flex flex-col lg:flex-row lg:flex-wrap items-center lg:items-start lg:justify-center justify-start lg:gap-[15vh] gap-10  overflow-y-scroll'>
                 {
                     handPickedProjects.map((item, index) => <HandPickedProjectCard key={index} {...item} />)
