b904c11543890cae91aa4c20dac00af0b985facf
Kanna116 <muralikanna77@gmail.com>
2024-03-08 13:35:09 +0530

    techstackbar theme done

--- pages/components/techstackbar.js
+++ pages/components/techstackbar.js
@@ -3,12 +3,12 @@ import { techStackIcons } from '../constants'
 
 function TechStackbar() {
     return (
-        <div className='w-screen h-fit py-5 lg:px-10 px-3 flex items-center mb-3 flex-wrap gap-x-[10%] md:gap-x-[15%] gap-y-5 lg:gap-x-[0%] lg:gap-y-0 justify-between border-[#fffffff] border-y-[1px]  -mx-[24px] md:-mx-[64px] lg:-mx-[96px]'>
+        <div className='w-screen h-fit py-5 lg:px-10 px-3 flex items-center mb-3 flex-wrap gap-x-[10%] md:gap-x-[15%] gap-y-5 lg:gap-x-[0%] lg:gap-y-0 justify-between border-secondary border-y-[1px]  -mx-[24px] md:-mx-[64px] lg:-mx-[96px]'>
             {
                 techStackIcons.map((item, index) => <span
                     key={index}
                     title={item.title}
-                    className='text-2xl'
+                    className='text-2xl text-primary'
                 >
                     {item.icon}
                 </span>)
