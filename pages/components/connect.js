ca5156f8f59150b9c57cd95365695735a3e3fa1e
Kanna116 <muralikanna77@gmail.com>
2024-03-08 13:47:13 +0530

    connect in homepage theme variations done

--- pages/components/connect.js
+++ pages/components/connect.js
@@ -4,12 +4,12 @@ import React from 'react'
 const Connect = () => {
     return (
         <div className='h-screen w-full flex items-center justify-center gap-10 flex-col sticky top-0'>
-            <h1 className='text-6xl text-center'>Let&apos;s Work Together</h1>
+            <h1 className='lg:text-6xl md:text-4xl text-3xl text-center'>Let&apos;s Work Together</h1>
             <div className='flex gap-5 items-center'>
                 <Link href={`/contact`} className='w-fit h-fit'>
-                    <button className='cursor-pointer  text-black bg-white px-[30px] py-[10px] rounded-[3px] text-base font-semibold'>Connect</button>
+                    <button className='cursor-pointer  text-secondary bg-secondary px-[30px] py-[10px] rounded-[3px] text-base font-semibold'>Connect</button>
                 </Link>
-                <button className='cursor-pointer text-white border-white border-[1px] px-[30px] py-[10px] rounded-[3px] text-base font-semibold'>Mail</button>
+                <button className='cursor-pointer text-primary border-secondary border-[1px] px-[30px] py-[10px] rounded-[3px] text-base font-semibold'>Mail</button>
             </div>
         </div>
     )
