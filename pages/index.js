23f8324246e571e420775e86a5c2ae4eae10c74a
Kanna116 <muralikanna77@gmail.com>
2024-03-09 10:54:53 +0530

    imported the locomotive scroll

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import HomePage from './home'
 import Head from 'next/head'
+import LocomotiveScroll from 'locomotive-scroll';
 
 const Home = () => {
+
+const locomotiveScroll = new LocomotiveScroll();
+
+
+
   return (
     <>
     <Head>
